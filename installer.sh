5e3337c1d4a4dc6e2e27e1ad7a3a3be02c8bb6f4
biko-73 <57875905+biko-73@users.noreply.github.com>
2024-09-24 14:03:50 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='BouquetMakerXtream/main'
 
-MY_IPK="enigma2-plugin-extensions-bouquetmakerxtream_1.21_all.ipk"
-MY_DEB="enigma2-plugin-extensions-bouquetmakerextream_1.21_all.deb"
+MY_IPK="enigma2-plugin-extensions-bouquetmakerxtream_1.25_all.ipk"
+MY_DEB="enigma2-plugin-extensions-bouquetmakerextream_1.25_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
