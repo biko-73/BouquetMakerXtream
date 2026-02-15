986235dabb4c5f01ff98950862e26d482345a99c
biko-73 <57875905+biko-73@users.noreply.github.com>
2026-02-15 10:41:26 +0200

    Update package version to 1.72

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='BouquetMakerXtream/main'
 
-MY_IPK="enigma2-plugin-extensions-bouquetmakerxtream_1.71_all.ipk"
-MY_DEB="enigma2-plugin-extensions-bouquetmakerxtream_1.71_all.deb"
+MY_IPK="enigma2-plugin-extensions-bouquetmakerxtream_1.72_all.ipk"
+MY_DEB="enigma2-plugin-extensions-bouquetmakerxtream_1.72_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
